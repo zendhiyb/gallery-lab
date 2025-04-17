34c0c7ae31bfa14d2f372a5d307664417d86a7af
Zendhi Yuna Burnama <zendhiyb10@gmail.com>
2025-04-17 20:52:49 +0700

    + update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,25 @@ window.addEventListener("scroll", () => {
         nav.classList.remove("sticky");
     }
 });
+
+const cursor = document.querySelector(".cursor");
+
+let mouseX = 0, mouseY = 0;
+let posX = 0, posY = 0;
+
+document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+function animate() {
+    posX += (mouseX - posX) * 0.20;
+    posY += (mouseY - posY) * 0.20;
+
+    cursor.style.left = posX + "px";
+    cursor.style.top = posY + "px";;
+
+    requestAnimationFrame(animate);
+}
+
+animate();
